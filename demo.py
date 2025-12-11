136ed5cce144a8d0f7ab42b3d78b52a538fa2c66
dmolinag1111 <dmolinag1111@gmail.com>
2025-12-11 12:20:39 +0000

    coding

--- demo.py
+++ demo.py
@@ -1,6 +1,20 @@
+import cudf.pandas
+cudf.pandas.install()
 import pandas as pd
 import streamlit as st
 import numpy as np 
 
 st.title('uber pickups in NYC')
+DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
+            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 
+date_column = 'date/time'
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows= nrows)
+    lowrcase = lamba x: str(x).lower()
+    data.rename(lowrcase, axis='columns', inplace= True)
+    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    
+data_load_state = st,text('Data loading...')
+data = load_state(100000)
+data_load_state.text('done!')
